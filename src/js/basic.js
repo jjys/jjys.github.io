d1d40d959d610a7bc975a28512ed98c1b37d9f44
unknown <jys922@gmail.com>
2021-06-23 14:10:51 +0800

    Fix the .on load function.

--- src/js/basic.js
+++ src/js/basic.js
@@ -119,7 +119,7 @@ function processRegion(region, regionType) {
 // Load large page
 function loadLargePage(page, pageElement) {	
 	var img = $('<img />');
-	img.load(function() {
+	img.on('load', function() {
 		var prevImg = pageElement.find('img');
 		$(this).css({width: '100%', height: '100%'});
 		$(this).appendTo(pageElement);
